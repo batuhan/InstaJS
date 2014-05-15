3f0052b905ff0c89e1f6bec6281de1b135205c32
Batuhan Icoz <batuhanicoz@gmail.com>
2014-05-15 22:48:19 +0300

    more semantic redirect parameter & make sure scope is filled

--- server/node/app.js
+++ server/node/app.js
@@ -7,11 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.get('/go', function(req, res){
-    res.redirect("https://api.instagram.com/oauth/authorize/?response_type=token&redirect_uri=" + encodeURIComponent(req.protocol + "://" + req.get('host') + "/cb?url=" + req.query.redirect_uri) + "&scope=" + req.query.scope + "&client_id=" + req.query.client_id)
+	if(!req.query.scope) req.query.scope = 'basic';
+	
+    res.redirect("https://api.instagram.com/oauth/authorize/?response_type=token&redirect_uri=" + encodeURIComponent(req.protocol + "://" + req.get('host') + "/cb?redirect_uri=" + req.query.redirect_uri) + "&scope=" + req.query.scope + "&client_id=" + req.query.client_id)
 });
 
 app.get('/cb', function(req, res){
-    res.send("<html><body><script>window.location = decodeURIComponent(window.location.search.split('?url=')[1]) + window.location.hash;</script></body></html>");
+    res.send("<html><body><script>window.location = decodeURIComponent(window.location.search.split('?redirect_uri=')[1]) + window.location.hash;</script></body></html>");
 });
 
 app.set('port', process.env.PORT || 3000);
